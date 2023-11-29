45ff482edbae402b86c72144990bf85f37b39e97
naiemhosen430 <naiemhosen430@gmail.com>
2023-11-29 06:14:35 -0800

    d

--- app/components/Shared/components/MainMav.jsx
+++ app/components/Shared/components/MainMav.jsx
@@ -1,6 +1,14 @@
 "use client";
 import Link from "next/link";
 import React, { useState } from "react";
+import {
+  IoLogoFacebook,
+  IoLogoInstagram,
+  IoLogoLinkedin,
+  IoLogoTwitter,
+  IoLogoYoutube,
+  IoMdReorder,
+} from "react-icons/io";
 
 export default function MainMav() {
   const [showBox, setShowBox] = useState(false);
@@ -92,67 +100,84 @@ export default function MainMav() {
       </div>
 
       {menuBox && (
-        <ul class="h-screen w-screen z-10 bg-slate-900">
-          <button
-            className="hover:text-slate-600 block py-2 text-white px-4 font-bold"
-            type="button"
-          >
-            <Link
-              onClick={() => {
-                closeSubMenu();
-                toggleMenuBox();
-              }}
-              href={"/"}
+        <div className="p-10 px-0">
+          <ul class="h-screen w-screen z-10 bg-slate-200">
+            <button
+              className="hover:text-slate-600 block py-2 text-black px-4 font-bold"
+              type="button"
             >
-              Home
-            </Link>
-          </button>
+              <Link
+                onClick={() => {
+                  closeSubMenu();
+                  toggleMenuBox();
+                }}
+                href={"/"}
+              >
+                Home
+              </Link>
+            </button>
 
-          <button
-            className="hover:text-slate-600 block py-2 text-white px-4 font-bold"
-            type="button"
-          >
-            <Link
-              onClick={() => {
-                closeSubMenu();
-                toggleMenuBox();
-              }}
-              href={"/about"}
+            <button
+              className="hover:text-slate-600 block py-2 text-black px-4 font-bold"
+              type="button"
             >
-              About
-            </Link>
-          </button>
+              <Link
+                onClick={() => {
+                  closeSubMenu();
+                  toggleMenuBox();
+                }}
+                href={"/about"}
+              >
+                About
+              </Link>
+            </button>
 
-          <button
-            className="hover:text-slate-600 block py-2 text-white px-4 font-bold"
-            type="button"
-          >
-            <Link
-              onClick={() => {
-                closeSubMenu();
-                toggleMenuBox();
-              }}
-              href={"/shop"}
+            <button
+              className="hover:text-slate-600 block py-2 text-black px-4 font-bold"
+              type="button"
             >
-              Shop
-            </Link>
-          </button>
+              <Link
+                onClick={() => {
+                  closeSubMenu();
+                  toggleMenuBox();
+                }}
+                href={"/shop"}
+              >
+                Shop
+              </Link>
+            </button>
 
-          <button
-            className="hover:text-slate-600 block py-2 text-white px-4 font-bold"
-            type="button"
-          >
-            <Link
-              onClick={() => {
-                closeSubMenu();
-                toggleMenuBox();
-              }}
-              href={"/contact"}
+            <button
+              className="hover:text-slate-600 block py-2 text-black px-4 font-bold"
+              type="button"
             >
-              Contact
-            </Link>
-          </button>
-        </ul>
+              <Link
+                onClick={() => {
+                  closeSubMenu();
+                  toggleMenuBox();
+                }}
+                href={"/contact"}
+              >
+                Contact
+              </Link>
+            </button>
+            <div className="flex items-center pt-20 justify-center space-x-5">
+              <IoLogoFacebook className="text-3xl inline-block text-black font-bold rounded-full" />
+              <IoLogoInstagram className="text-3xl inline-block text-black font-bold rounded-full" />
+              <IoLogoTwitter className="text-3xl inline-block text-black font-bold rounded-full" />
+              <IoLogoLinkedin className="text-3xl inline-block text-black font-bold rounded-full" />
+              <IoLogoYoutube className="text-3xl inline-block text-black font-bold rounded-full" />
+            </div>
+            <div className="text-center py-5">
+              <h1 className="font-bold">+8801920679575</h1>
+              <h1 className="font-bold">naiemhosen430@gmail.com</h1>
+              <p className="p-5">
+                ShopNest is your one-stop destination for all your online
+                shopping
+              </p>
+            </div>
+          </ul>
+        </div>
       )}
 
       {showBox && <SubMenuBox />}
